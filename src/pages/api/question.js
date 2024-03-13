8363280b29b2352fa809e752b72298d34b3bbd0b
isromero <ismaromeroortega@gmail.com>
2024-03-13 16:29:42 +0100

    Finish logic, similarity with embeddings, cache database...

--- src/pages/api/question.js
+++ src/pages/api/question.js
@@ -2,14 +2,29 @@ import OpenAI from "openai";
 import { query } from "@/lib/db";
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
+const SIMILARITY_RANGE = 0.8;
+
+const cosineSimilarity = (vecA, vecB) => {
+  const dotProduct = vecA.reduce((acc, val, idx) => acc + val * vecB[idx], 0);
+  const magnitudeA = Math.sqrt(vecA.reduce((acc, val) => acc + val ** 2, 0));
+  const magnitudeB = Math.sqrt(vecB.reduce((acc, val) => acc + val ** 2, 0));
+  return dotProduct / (magnitudeA * magnitudeB);
+};
+
+const getRowsOfDB = async() => {
+  const result = await query(
+    `SELECT question, answer, embedding FROM data;`
+  )
+  return result.rows;
+};
 
 const getEmbedding = async (question) => {
   const embedding = await openai.embeddings.create({
-    model: 'text-embedding-ada-002',
+    model: "text-embedding-ada-002",
     input: question,
-    encoding_format: 'float'
+    encoding_format: "float",
   });
-  console.log(embedding.data[0].embedding)
+
   return embedding.data[0].embedding;
 };
 
@@ -25,25 +40,35 @@ export default async function handler(req, res) {
   }
 
   try {
-    const completionResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-16k",
-      messages: [{ role: "user", content: question }],
-      max_tokens: 10,
-      temperature: 0.5,
-      n: 1
-    });
-    const answer = completionResponse.choices[0].message.content;
-    const embedding = (await getEmbedding(question));
+    const actualEmbedding = await getEmbedding(question);
+    const rowsDB = await getRowsOfDB();
+    let answer = '';
+    let isSimilar = false;
 
-    const insertResult = await query(
-      'INSERT INTO data(question, answer, embedding) VALUES($1, $2, $3) RETURNING *', 
-      [question, answer, '[' + embedding + ']']
-    );
+    for (const row of rowsDB) {
+      const similarity = cosineSimilarity(actualEmbedding, JSON.parse(row.embedding));
+      if (similarity > SIMILARITY_RANGE) {
+        answer = row.answer;
+        isSimilar = true;
+        break;
+      }
+    }
 
-    res.status(200).json({ question, answer, dbResult: insertResult.rows[0] });
+    if (!isSimilar) {
+      console.log('Nunca habías dicho nada parecido')
+      const completionResponse = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-16k",
+        messages: [{ role: "user", content: question }],
+        max_tokens: 10,
+        temperature: 0.5,
+        n: 1,
+      });
+      answer = completionResponse.choices[0].message.content;
+      await query("INSERT INTO data(question, answer, embedding) VALUES($1, $2, $3) RETURNING *", [question, answer, '[' + actualEmbedding + ']']);
+    }
+    res.status(200).json({ question, answer });
   } catch (error) {
     console.error("Error fetching response from OpenAI:", error);
     res.status(500).json({ error: "Failed to fetch response from OpenAI" });
   }
 };
-
