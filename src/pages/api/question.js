27ba53c413f09e4ab4cc1e6b5e2c99b78432816d
isromero <ismaromeroortega@gmail.com>
2024-03-13 15:24:20 +0100

    Get embedding of question

--- src/pages/api/question.js
+++ src/pages/api/question.js
@@ -1,6 +1,18 @@
 import OpenAI from "openai";
 import { query } from "@/lib/db";
 
+const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
+
+const getEmbedding = async (question) => {
+  const embedding = await openai.embeddings.create({
+    model: 'text-embedding-ada-002',
+    input: question,
+    encoding_format: 'float'
+  });
+  console.log(embedding.data[0].embedding)
+  return embedding.data[0].embedding;
+};
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end("Method Not Allowed");
@@ -12,22 +24,20 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "No question provided" });
   }
 
-  const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
-
   try {
     const completionResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k",
       messages: [{ role: "user", content: question }],
       max_tokens: 10,
       temperature: 0.5,
       n: 1
     });
-
-    const answer = completionResponse.data.choices[0].text.trim();
+    const answer = completionResponse.choices[0].message.content;
+    const embedding = (await getEmbedding(question));
 
     const insertResult = await query(
-      'INSERT INTO data(question, answer) VALUES($1, $2) RETURNING *', 
-      [question, answer]
+      'INSERT INTO data(question, answer, embedding) VALUES($1, $2, $3) RETURNING *', 
+      [question, answer, '[' + embedding + ']']
     );
 
     res.status(200).json({ question, answer, dbResult: insertResult.rows[0] });
